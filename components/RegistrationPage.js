c193129956dce38d0ffd6b3985bd75c213f39ee0
Kien Vu <kienvu@1x-193-157-199-15.uio.no>
2016-07-04 16:05:07 +0200

    MOB-348: Pasientregistrering nesten ferdig design

--- components/RegistrationPage.js
+++ components/RegistrationPage.js
@@ -20,30 +20,36 @@
 
 import React, { Component } from 'react';
 import {
+  SegmentedControlIOS,
+  ScrollView,
   Text,
   TextInput,
   View,
 } from 'react-native';
 
+const backgroundColor = "#FFFFFF"
 const textColor = "#FFFFFF"
 const lightTextColor = "#777777"
 const buttonColor = "rgb(33, 115, 161)"
 const inputFieldColor = "rgb(246, 246, 246)"
-const buttonWidth = 200
+const buttonWidth = 250
 
 class RegistrationPage extends Component {
   render() {
     return (
-      <View>
+      <View style={{flex: 1}}>
         <NavigationBar />
-        <Personalia /><Divider />
-        <Anthropometry /><Divider />
-        <Needs/><Divider />
-        <Screening/><Divider />
-        <SpecialDiet/><Divider />
-        <Preferences/>
-
-        <Button text="Registrer" />
+        <ScrollView style={{paddingTop: 20}}>
+          <Personalia /><Divider />
+          <Anthropometry /><Divider />
+          <Needs/><Divider />
+          <Screening/><Divider />
+          <SpecialDiet/><Divider />
+          <Preferences/>
+          <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+            <Button text="Registrer" />
+          </View>
+        </ScrollView>
       </View>
     );
   }
@@ -52,9 +58,9 @@ class RegistrationPage extends Component {
 class Personalia extends Component {
   render() {
     return (
-      <View>
+      <Section>
         <Header text="Personalia" />
-        <Question name="navn">
+        <Question name="Navn">
           {
             ["Fornavn", "Mellomnavn", "Etternavn"].map(
               name => <InputField key={name} placeholder={name} />)
@@ -64,12 +70,12 @@ class Personalia extends Component {
           <Choice choices={["Kvinne", "Mann"]} />
         </Question>
         <Question name="Alder">
-          <InputField />
+          <InputField small={true}/>
         </Question>
         <Question name="Fødselsnummer">
           <InputField />
         </Question>
-      </View>
+      </Section>
     );
   }
 }
@@ -77,16 +83,24 @@ class Personalia extends Component {
 class Anthropometry extends Component {
   render() {
     return (
-      <View>
+      <Section>
         <Header text="Antropometri" />
-        <Question name="Høyde">
-          <InputField />
-        </Question>
-        <Question name="Vekt">
-          <InputField />
-        </Question>
-        <Calculation name="KMI" />
+        <View style={{flexDirection: 'row'}}>
+          <View style={{flex: 1}}>
+            <Question name="Høyde">
+              <InputField small={true}/>
+            </Question>
+          </View>
+          <View style={{flex: 1}}>
+          <Question name="Vekt">
+            <InputField small={true}/>
+          </Question>
+          </View>
+          <View style={{flex: 1}}>
+          <Calculation name="KMI" />
+          </View>
         </View>
+        </Section>
     );
   }
 }
@@ -94,12 +108,20 @@ class Anthropometry extends Component {
 class Needs extends Component {
   render() {
     return (
-      <View>
+      <Section>
       <Header text="Behov" />
-      <Calculation name="Energi" />
-      <Calculation name="Protein" />
-      <Calculation name="Væske" />
+      <View style={{flexDirection: 'row'}}>
+        <View style={{flex: 1}}>
+          <Calculation name="Energi" />
+        </View>
+        <View style={{flex: 1}}>
+          <Calculation name="Protein" />
+        </View>
+        <View style={{flex: 1}}>
+          <Calculation name="Væske" />
+        </View>
       </View>
+      </Section>
     );
   }
 }
@@ -107,7 +129,7 @@ class Needs extends Component {
 class Screening extends Component {
   render() {
     return (
-      <View>
+      <Section>
       <Header text="Screening" />
       <Question name="Score screening">
         <InputField />
@@ -115,7 +137,7 @@ class Screening extends Component {
       <Question name="Ernæringsmessig risiko">
         <Choice choices={["Moderat", "Gøy"]} />
       </Question>
-      </View>
+      </Section>
     );
   }
 }
@@ -123,13 +145,13 @@ class Screening extends Component {
 class SpecialDiet extends Component {
   render() {
     return (
-      <View>
+      <Section>
       <Header text="Spesialkost" />
       <Question>
         <Choice choices={["Nei", "Ja"]} />
         <InputField placeholder="Spesifiser type spesialkost" />
       </Question>
-      </View>
+      </Section>
     );
   }
 }
@@ -137,53 +159,107 @@ class SpecialDiet extends Component {
 class Preferences extends Component {
   render() {
     return (
-      <View>
+      <Section>
       <Header text="Spesielle preferanser" />
       <Question>
         <InputField />
       </Question>
-      </View>
+      </Section>
     );
   }
 }
 
+const Section = ({children}) => (
+  <View style={{marginLeft: 30, marginRight: 30}}>
+    {children}
+  </View>
+);
+
 const Header = ({text}) => (
-  <Text>
+  <Text style={{fontSize: 30, fontWeight: 'bold', marginTop: 32, marginBottom: 20}}>
     {text}
   </Text>
 );
 
 const Button = ({text}) => (
   <View style={{marginBottom: 15}}>
-  <Text style={{color: 'white', backgroundColor: buttonColor, paddingLeft: 20,
-                paddingTop: 8, paddingRight: 20, paddingBottom: 8, fontStyle: 'italic',
+  <Text style={{marginTop: 60, fontSize: 25, color: 'white', backgroundColor: buttonColor, paddingLeft: 20,
+                paddingTop: 16, paddingRight: 20, paddingBottom: 16, fontStyle: 'italic',
                 borderRadius: 5, overflow: 'hidden', width: buttonWidth, textAlign: 'center'}}>
     {text}
   </Text>
   </View>
 )
 
-const Question = ({text}) => (
-  <Text>
-    {text}
-  </Text>
-);
-
-const Calculation = ({text}) => (
-  <Text>
-    {text}
-  </Text>
-);
-
-const NavigationBar = () => <Text>Pasientregistrering</Text>;
-const Divider = () => <Text>***</Text>;
-const Choice = ({choices}) => choices.map(name => <Text>{name}</Text>);
-
-const InputField = ({placeholder}) => (
+const Question = ({name, children, style}) => (
   <View>
-    <TextInput placeholder={placeholder}
-               style={{width: buttonWidth, height: 40, backgroundColor: inputFieldColor,
-                       borderRadius: 5, fontSize: 13, padding: 10, marginBottom: 15}}/>
+  {name &&
+    <Text style={[{fontSize: 25, marginBottom: 20}, style]}>
+      {name}:
+    </Text>
+  }
+  {children}
+  </View>
+);
+
+const Calculation = ({name}) => (
+  <View>
+    <Text style={{fontSize: 25, marginBottom: 20}}>
+      {name}:
+    </Text>
+    <Text style={{fontSize: 25}}>-</Text>
+
+  </View>
+);
+
+const NavigationBar = () => (
+  <View style={{backgroundColor: "#17364B", paddingTop: 50, paddingBottom: 30, paddingLeft: 20, paddingRight: 20}}>
+    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+      <View style={{flex: 1}}>
+        <BackLink />
+      </View>
+      <View style={{flex: 2}}>
+
+      </View>
+      <View style={{flex: 1}}>
+        <FrontPageLink />
+      </View>
+    </View>
+
+    <Text style={{color: 'white', textAlign: 'center', fontSize: 35, fontWeight: 'bold', marginTop: 25}}>
+      Pasientregistrering
+    </Text>
+  </View>
+);
+
+const BackLink = () => (
+  <Text style={{color: 'white', fontSize: 25, textAlign: 'center'}}>
+  Tilbake
+  </Text>
+);
+
+const FrontPageLink = () => (
+  <Text style={{color: 'white', fontSize: 25, textAlign: 'center'}}>
+  Til forsiden
+  </Text>
+);
+
+const Divider = () => <View style={{marginTop: 30, height: 6, backgroundColor: "rgb(235, 235, 235)"}}/>;
+
+const Choice = ({choices}) => (
+  <View>
+    <SegmentedControlIOS values={choices}
+      style={{height: 30, marginBottom: 10, padding: 20}}
+      tintColor= '#17364B'/>
+  </View>
+);
+
+const InputField = ({placeholder, small}) => (
+  <View>
+    <TextInput placeholder={placeholder || ""}
+               style={{width: small ? 120 : null, height: 60, backgroundColor: inputFieldColor,
+                      borderRadius: 8, fontSize: 25, padding: 10, marginBottom: 15,
+                     borderColor: "rgb(215, 215, 215)", borderWidth: 3}}/>
   </View>
 );
 
