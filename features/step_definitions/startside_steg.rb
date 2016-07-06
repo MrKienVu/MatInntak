ff4112778b2541958731091db5b5bb9f6e36ddcb
Kien Vu <kien.vu@usit.uio.no>
2016-07-06 11:15:30 +0200

    MOB-348: funksjonell test passerer

--- features/step_definitions/startside_steg.rb
+++ features/step_definitions/startside_steg.rb
@@ -4,53 +4,10 @@ Gitt(/^at pasienten .*har brukt applikasjonen.*$/) do
   end
 end
 
-Når(/^knappen for ([^"]*) trykkes på$/) do |button|
+Når(/^knappen ([^"]*) trykkes på$/) do |button|
   touch("* marked:'#{button}'")
 end
 
-Så(/^skal skjermbildet for pasientregistrering vises$/) do
-  for item in ["Pasientregistrering",
-               "Personalia", "Antropometri",
-               "Behov", "Screening", "Spesialkost"] do
-    wait_for_text(item, timeout: 5)
-  end
-end
-
-Og(/^jeg registrerer meg som "([^"]*)"$/) do |test|
-
-  touch("* marked:'studieId'")
-  wait_for_keyboard
-  keyboard_enter_text("test")
-
-  touch("* marked:'gjentaStudieId'")
-  wait_for_keyboard
-  keyboard_enter_text("test")
-
-  wait_for_element_exists("* marked:'Neste'")
-  touch("* marked:'Neste'")
-
-  if element_exists("* marked:'Gi tilgang'")
-    touch("* marked:'Gi tilgang'")
-    wait_for_element_exists("* marked:'OK'")
-    touch("* marked:'OK'")
-  end
-
-  wait_for_element_exists("* marked:'Neste'")
-  touch("* marked:'Neste'")
-
-  touch("* marked:'Ønsker ikke app kode'")
-  wait_for_element_exists("* marked:'Fortsett uten app kode'")
-  touch("* marked:'Fortsett uten app kode'")
-
-end
-
-Så(/^vil et ukentlig skjema være tilgjengelig$/) do
-
-  wait_for_element_exists("* marked:'Ukentlig skjema'")
-  weeklySurveyExist = true
-
-  unless weeklySurveyExist
-    fail  "Ukentlig skjema ikke tilgjengelig"
-  end
-
+Så(/^skal skjermbildet ([^"]*) vises$/) do |screenHeading|
+  wait_for_element_exists("* marked: '#{screenHeading}'", timeout: 5)
 end
